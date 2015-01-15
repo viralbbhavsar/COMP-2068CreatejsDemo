ba21fcad87059148abe967ef4b980655bf0c962b
viralbbhavsar <viralbbhavsar@gmail.com>
2015-01-15 13:43:56 -0500

    added visual studio file

---
+++ COMP2068-CreatejsDEmo/Scripts/game.ts
@@ -0,0 +1,19 @@
+﻿var canvas;
+var stage: createjs.Stage;
+
+function init() {
+    canvas = document.getElementById("firstgame");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+}
+function gameLoop() {
+        stage.update();
+}
+function main() {
+    var helloText = new createjs.Text("Hello World", "40px Consolas", "#000000");
+    stage.addChild(helloText);
+}
+
+
